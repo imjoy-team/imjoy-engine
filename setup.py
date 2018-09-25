4b7374d3effc0a2546fd9afe14c7a31e7e5a7a2d
Wei Ouyang <oeway007@gmail.com>
2018-09-25 15:46:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except OSError as e:
 
 requirements = []
 if sys.version_info > (3, 0):
-    requirements = ['psutil', 'requests', 'six', 'websocket-client-py3', 'aiohttp', 'numpy']
+    requirements = ['psutil', 'requests', 'six', 'websocket-client-py3', 'aiohttp', 'numpy', 'python-socketio']
 
 from setuptools import setup, find_packages
 setup(name='imjoy',
