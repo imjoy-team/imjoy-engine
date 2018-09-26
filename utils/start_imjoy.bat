00331dc2e322aec881455409e3d2dff06cb1027b
aaristov <andrey.aristov@pasteur.fr>
2018-09-26 17:31:51 +0200

    add start imjoy for windows

---
+++ utils/start_imjoy.bat
@@ -0,0 +1,19 @@
+@setlocal enableextensions
+@cd /d "%~dp0"
+if not exist %systemdrive%%homepath%\ImJoyApp\Scripts\conda.exe GOTO InstallImJoyApp
+powershell -command "& {$env:Path = '%systemdrive%%homepath%\ImJoyApp;%systemdrive%%homepath%\ImJoyApp\Scripts;' + $env:Path ; python -m imjoy}"
+IF NOT %ERRORLEVEL%==0 GOTO InstallImJoyApp
+pause
+goto:eof
+
+:InstallImJoyApp
+  Echo Installing ImJoy App...
+  pause
+  powershell Set-ExecutionPolicy RemoteSigned
+  IF NOT %ERRORLEVEL%==0 GOTO RequirePermission
+  powershell -file Windows_Install.ps1
+  pause
+  goto:eof
+:RequirePermission
+  Echo Please run this script as Administrator!
+  pause
