1d4fc8dea12a70ee952bf19035f51432b1219683
Wei OUYANG <oeway007@gmail.com>
2018-09-27 11:20:47 +0200

    output thread

--- imjoy/pluginEngine.py
+++ imjoy/pluginEngine.py
@@ -460,51 +460,8 @@ def execute(requirements_cmd, args, work_dir, abort, name, plugin_env):
     else:
         logger.info('error occured during terminating a process, code: ' + str(ProcessException(command, exitCode, output)))
 
-class NonBlockingStreamReader:
-
-    def __init__(self, stream):
-        '''
-        stream: the stream to read from.
-                Usually a process' stdout or stderr.
-        '''
-        self._s = stream
-        self._q = Queue()
-
-        def _populateQueue(stream, queue):
-            '''
-            Collect lines from 'stream' and put them in 'quque'.
-            '''
-
-            while True:
-                line = stream.readline()
-                if line:
-                    queue.put(line)
-                else:
-                    self.end = True
-                    break
-                    #raise UnexpectedEndOfStream
-                time.sleep(0.01)
-        self.end = False
-        self._t = threading.Thread(target=_populateQueue,
-                                   args=(self._s, self._q))
-        self._t.daemon = True
-        self._t.start()  # start collecting lines from the stream
-
-    def readline(self, timeout=None):
-        try:
-            return self._q.get(block=timeout is not None,
-                               timeout=timeout)
-        except Empty:
-            if self.end:
-                raise UnexpectedEndOfStream
-            return None
-
-class UnexpectedEndOfStream(Exception):
-    pass
-
-
-
 print('======>> Connection Token: '+opt.token + ' <<======')
+
 try:
     web.run_app(app, host=opt.host, port=opt.port)
 finally:
