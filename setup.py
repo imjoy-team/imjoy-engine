46a54667dbfde75fa79cea4d5d865cfa39fad01c
Wei OUYANG <oeway007@gmail.com>
2018-10-10 15:47:56 +0200

    add psutil conda fallback

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import sys
 import subprocess
 
+PSUTIL_INSTALLED = False
 try:
     subprocess.call(["conda", "-V"])
-    CONDA_AVAILABLE = True
+    try:
+        print('Trying to install psutil with conda...')
+        subprocess.call(["conda", "install", "-y", "psutil"])
+        PSUTIL_INSTALLED = True
+    except Exception as e:
+        print("Failed to install psutil.")
 except OSError as e:
     if sys.version_info > (3, 0):
         print('WARNING: you are running ImJoy without conda, you may have problem with some plugins.')
@@ -11,32 +17,21 @@ except OSError as e:
         sys.exit('Sorry, ImJoy plugin engine can only run within a conda environment or at least in Python 3.')
 
 from setuptools import setup, find_packages
+
 requirements = []
+if sys.version_info > (3, 0):
+    requirements = ['aiohttp', 'python-socketio', 'requests', 'six', 'websocket-client', 'numpy']
+    if not PSUTIL_INSTALLED:
+        requirements.append("psutil")
 
-def run_setup():
-    setup(name='imjoy',
-          version='0.6.7',
-          description='Python plugin engine for ImJoy.io',
-          url='http://github.com/oeway/ImJoy',
-          author='Wei OUYANG',
-          author_email='wei.ouyang@cri-paris.org',
-          license='MIT',
-          packages=find_packages(),
-          include_package_data=True,
-          install_requires=requirements,
-          zip_safe=False)
-
-try:
-    if sys.version_info > (3, 0):
-        requirements = ['psutil', 'aiohttp', 'python-socketio', 'requests', 'six', 'websocket-client', 'numpy']
-        run_setup()
-except Exception as e:
-    # some OSs do not have psutil with pip, try to install the dependencies with conda
-    if sys.version_info > (3, 0) and CONDA_AVAILABLE:
-        try:
-            print('Trying to install psutil with conda...')
-            subprocess.call(["conda", "install", "-y", "psutil"])
-        except Exception as e:
-            print("Failed to install psutil.")
-        else:
-            run_setup()
+setup(name='imjoy',
+      version='0.6.7',
+      description='Python plugin engine for ImJoy.io',
+      url='http://github.com/oeway/ImJoy',
+      author='Wei OUYANG',
+      author_email='wei.ouyang@cri-paris.org',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=requirements,
+      zip_safe=False)
