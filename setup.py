2c6f6de63057a4fa584328d0685b06e14f748b3c
Wei OUYANG <oeway007@gmail.com>
2018-10-10 15:42:04 +0200

    add psutil conda fallback

--- setup.py
+++ setup.py
@@ -13,9 +13,7 @@ except OSError as e:
 from setuptools import setup, find_packages
 requirements = []
 
-try:
-    if sys.version_info > (3, 0):
-        requirements = ['psutil', 'aiohttp', 'python-socketio', 'requests', 'six', 'websocket-client', 'numpy']
+def run_setup():
     setup(name='imjoy',
           version='0.6.7',
           description='Python plugin engine for ImJoy.io',
@@ -27,24 +25,18 @@ try:
           include_package_data=True,
           install_requires=requirements,
           zip_safe=False)
+
+try:
+    if sys.version_info > (3, 0):
+        requirements = ['psutil', 'aiohttp', 'python-socketio', 'requests', 'six', 'websocket-client', 'numpy']
+        run_setup()
 except Exception as e:
     # some OSs do not have psutil with pip, try to install the dependencies with conda
     if sys.version_info > (3, 0) and CONDA_AVAILABLE:
         try:
-            print('Trying to install package with conda...')
-            requirements = ['psutil', 'aiohttp', 'python-socketio', 'requests', 'six', 'websocket-client', 'numpy']
-            subprocess.call(["conda", "install"] + requirements)
+            print('Trying to install psutil with conda...')
+            subprocess.call(["conda", "install", "-y", "psutil"])
         except Exception as e:
-            print("Failed to install dependencies.")
-        finally:
-            setup(name='imjoy',
-                  version='0.6.7',
-                  description='Python plugin engine for ImJoy.io',
-                  url='http://github.com/oeway/ImJoy',
-                  author='Wei OUYANG',
-                  author_email='wei.ouyang@cri-paris.org',
-                  license='MIT',
-                  packages=find_packages(),
-                  include_package_data=True,
-                  install_requires=requirements,
-                  zip_safe=False)
+            print("Failed to install psutil.")
+        else:
+            run_setup()
