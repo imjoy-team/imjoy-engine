b2b2b8b0721584371d2b88e1029d7741529f1a26
oeway <oeway007@gmail.com>
2019-01-03 14:01:13 +0100

    add janus as dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except OSError as e:
 
 requirements = []
 if sys.version_info > (3, 0):
-    requirements = ['aiohttp', 'python-socketio', 'requests', 'six', 'websocket-client', 'numpy', "psutil"]
+    requirements = ['aiohttp', 'python-socketio', 'requests', 'six', 'websocket-client', 'numpy', 'psutil', 'janus']
 try:
     setup(name='imjoy',
           version='0.7.7',
