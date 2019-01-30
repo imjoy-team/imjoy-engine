14ba39dd22eb1ab319d5a4fd674a0d6d13fae6cc
Wei OUYANG <oeway007@gmail.com>
2019-01-30 11:22:52 +0100

    try to install psutil first

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 
 try:
     subprocess.call(["conda", "-V"])
-
 except OSError as e:
     if sys.version_info > (3, 0):
         print('WARNING: you are running ImJoy without conda, you may have problem with some plugins.')
@@ -13,19 +12,24 @@ except OSError as e:
 
 requirements = []
 if sys.version_info > (3, 0):
-    requirements = ['aiohttp', 'python-socketio', 'requests', 'six', 'websocket-client', 'numpy', 'psutil', 'janus', 'pyyaml']
-try:
-    setup(name='imjoy',
-          version='0.7.12',
-          description='Python Plugin Engine for ImJoy.io',
-          url='http://github.com/oeway/ImJoy',
-          author='Wei OUYANG',
-          author_email='wei.ouyang@cri-paris.org',
-          license='MIT',
-          packages=find_packages(),
-          include_package_data=True,
-          install_requires=requirements,
-          zip_safe=False)
-except Exception as e:
-    print('Trying to install psutil with conda...')
-    subprocess.call(["conda", "install", "-y", "psutil"])
+    requirements = ['aiohttp', 'python-socketio', 'requests', 'six', 'websocket-client', 'numpy', 'janus', 'pyyaml']
+
+    print('Trying to install psutil with pip...')
+    ret = subprocess.Popen(['pip', 'install', 'psutil'], shell=False).wait()
+    if ret != 0:
+        print('Trying to install psutil with conda...')
+        ret2 = subprocess.Popen(["conda", "install", "-y", "psutil"]).wait()
+        if ret2 != 0:
+            raise Exception('Failed to install psutil, please try to setup an environment with gcc support.')
+
+setup(name='imjoy',
+      version='0.7.13',
+      description='Python Plugin Engine for ImJoy.io',
+      url='http://github.com/oeway/ImJoy',
+      author='Wei OUYANG',
+      author_email='wei.ouyang@cri-paris.org',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=requirements,
+      zip_safe=False)
