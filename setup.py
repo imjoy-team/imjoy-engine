7d4cb20e08dfdfc154deb669b488c254d88d4d68
oeway <oeway007@gmail.com>
2019-05-11 11:32:24 +0200

    bump version

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="imjoy",
-    version="0.7.61",
+    version="0.7.62",
     description="ImJoy Plugin Engine for running Python plugins locally or remotely from ImJoy.io",
     long_description=README,
     long_description_content_type="text/markdown",
