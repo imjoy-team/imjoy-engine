0354e529fc2bfbe247a4edad8021baacb17e7eac
oeway <oeway007@gmail.com>
2019-05-12 14:15:12 +0200

    upgrade twine wheel and setuptools; bump version

--- utils/publish.sh
+++ utils/publish.sh
@@ -1,4 +1,8 @@
-pip install twine
+pip install -U twine
+pip install -U wheel
+pip install -U setuptools
+rm -rf ./build
 rm ./dist/*
 python setup.py sdist bdist_wheel
 twine upload dist/*
+rm -rf ./build
