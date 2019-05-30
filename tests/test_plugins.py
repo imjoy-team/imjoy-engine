b73be27e3b1c95e4fb7a3e20fe195f78854fa172
Martin Hjelmare <marhje52@kth.se>
2019-05-30 16:15:54 +0200

    Use conftest.py and common.py
    * Move project fixtures to conftest.py and common test utils to
      common.py.

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,24 +1,8 @@
 """Test plugin engine api."""
-import signal
-import subprocess
-import time
-import uuid
-from pathlib import Path
-
 import pytest
 
-from .fake_client import FakeClient
-
-# pylint: disable=unused-argument
 # All test coroutines will be treated as marked.
 pytestmark = pytest.mark.asyncio  # pylint: disable=invalid-name
-
-HERE = Path(__file__).parent
-ENGINE_MODULE = HERE.parent / "imjoy/engine.py"
-HOST = "localhost"
-PORT = 9527
-TOKEN = "12345678"
-URL = f"http://{HOST}:{PORT}"
 
 TEST_PLUGIN_CONFIG = {
     "name": "test-plugin",
@@ -36,31 +20,6 @@ TEST_PLUGIN_CONFIG = {
     "requirements": "",
     "dependencies": [],
 }
-
-
-@pytest.fixture(name="engine")
-def setup_engine():
-    """Set up engine."""
-    engine_args = f"python {ENGINE_MODULE} --debug --token {TOKEN}"
-    process = subprocess.Popen(engine_args.split())
-    time.sleep(2)  # This is needed to let the engine finish setup.
-    yield
-    process.send_signal(signal.SIGINT)
-    try:
-        process.wait(timeout=10)
-    except subprocess.TimeoutExpired:
-        print("Waiting for engine exit timed out")
-
-
-@pytest.fixture(name="client")
-async def mock_client(engine, event_loop):
-    """Provide a mock client."""
-    client_id = str(uuid.uuid4())
-    session_id = str(uuid.uuid4())
-    client = FakeClient(URL, client_id, session_id, TOKEN, event_loop)
-    await client.connect()
-    await client.register_client()
-    yield client
 
 
 @pytest.fixture(name="init_plugin")
