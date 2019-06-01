6030895a89e0ecc4a5e658e7b0d810b6ce763371
oeway <oeway007@gmail.com>
2019-06-01 21:08:00 +0200

    update requirements

--- imjoy/const.py
+++ imjoy/const.py
@@ -2,9 +2,9 @@
 import json
 import pathlib
 
-DEFAULT_REQUIREMENTS = ["numpy"]
-DEFAULT_REQUIREMENTS_PY2 = [*DEFAULT_REQUIREMENTS, "python-socketio[client]"]
-DEFAULT_REQUIREMENTS_PY3 = [*DEFAULT_REQUIREMENTS, "python-socketio[asyncio_client]"]
+DEFAULT_REQUIREMENTS = ["numpy", "python-socketio[client]"]
+DEFAULT_REQUIREMENTS_PY2 = DEFAULT_REQUIREMENTS
+DEFAULT_REQUIREMENTS_PY3 = [*DEFAULT_REQUIREMENTS, "janus"]
 REQ_PSUTIL = ["psutil"]
 REQ_PSUTIL_CONDA = ["conda:psutil"]
 ENGINE = "imjoy_engine"
