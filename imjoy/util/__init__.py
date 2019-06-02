3010e0d86f0714f21f743f652156d9def73137d8
oeway <oeway007@gmail.com>
2019-06-02 16:54:33 +0200

    remove Registry from util

--- imjoy/util/__init__.py
+++ imjoy/util/__init__.py
@@ -17,22 +17,6 @@ if sys.platform == "win32":
         return drives
 
 
-class Registry(dict):
-    """Registry of items."""
-
-    # https://github.com/home-assistant/home-assistant/blob/
-    # 2a9fd9ae269e8929084e53ab12901e96aec93e7d/homeassistant/util/decorator.py
-    def register(self, name):
-        """Return decorator to register item with a specific name."""
-
-        def decorator(func):
-            """Register decorated function."""
-            self[name] = func
-            return func
-
-        return decorator
-
-
 def parse_repos(requirements, work_dir):
     """Return a list of repositories from a list of requirements."""
     repos = []
