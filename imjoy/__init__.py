755f7f0726c6c9a96ba9fafd5f1765a74bd10af4
oeway <oeway007@gmail.com>
2019-06-04 11:26:02 +0200

    use absolute path for IMJOY_PACKAGE_DIR

--- imjoy/__init__.py
+++ imjoy/__init__.py
@@ -4,7 +4,7 @@ import pathlib
 
 # read version information from file
 _here = pathlib.Path(__file__).parent
-IMJOY_PACKAGE_DIR = str(_here)
+IMJOY_PACKAGE_DIR = str(_here.absolute())
 VERSION_INFO = json.loads((_here / "VERSION").read_text())
 __version__ = VERSION_INFO["version"]
 API_VERSION = VERSION_INFO["api_version"]
