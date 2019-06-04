d3c3d120514894b8a4e6d092f6512d00f39c6d5c
oeway <oeway007@gmail.com>
2019-06-04 11:05:31 +0200

    use pathlib by default

--- imjoy/__init__.py
+++ imjoy/__init__.py
@@ -1,10 +1,10 @@
 """Package the ImJoy plugin engine."""
 import json
-import os
+import pathlib
 
 # read version information from file
-IMJOY_PACKAGE_DIR = os.path.dirname(__file__)
-with open(os.path.join(IMJOY_PACKAGE_DIR, "VERSION"), "r") as f:
-    VERSION_INFO = json.load(f)
-    __version__ = VERSION_INFO["version"]
-    API_VERSION = VERSION_INFO["api_version"]
+_here = pathlib.Path(__file__).parent
+IMJOY_PACKAGE_DIR = str(_here)
+VERSION_INFO = json.loads((_here / "VERSION").read_text())
+__version__ = VERSION_INFO["version"]
+API_VERSION = VERSION_INFO["api_version"]
