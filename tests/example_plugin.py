b903f9da9de072c32724ebef26fcdeefe8fdf723
Martin Hjelmare <marhje52@gmail.com>
2021-04-23 22:28:35 +0200

    Lint example plugin

--- tests/example_plugin.py
+++ tests/example_plugin.py
@@ -1,21 +1,28 @@
+"""Provide an example plugin."""
 from imjoy_rpc import api
-import asyncio
+
+# pylint: disable=no-self-use
 
 
 class ImJoyPlugin:
+    """Represent an ImJoy plugin."""
+
     def authorizer(self, user_info, context):
+        """Authorize users."""
         return True
 
     async def setup(self):
+        """Set up the plugin."""
         await api.registerService(
             {"name": "echo service", "echo": lambda x: print("echo: " + str(x))}
         )
-        s = await api.getService("echo service")
-        await s.echo("a message")
+        service = await api.getService("echo service")
+        await service.echo("a message")
         await api.log("initialized")
 
     async def run(self, ctx):
-        await api.log("hello world.")
+        """Run the plugin."""
+        await api.log("hello world")
 
 
 api.export(ImJoyPlugin(), config={"name": "test-plugin", "workspace": "123"})
