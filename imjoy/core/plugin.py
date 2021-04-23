f9adf024247ccf156edbfbc0f73f8c7b2d595261
Martin Hjelmare <marhje52@gmail.com>
2021-04-24 00:09:04 +0200

    Lint plugin

--- imjoy/core/plugin.py
+++ imjoy/core/plugin.py
@@ -1,3 +1,4 @@
+"""Provide a plugin interface."""
 import asyncio
 import logging
 import sys
@@ -13,11 +14,16 @@ logger.setLevel(logging.INFO)
 
 
 class DynamicPlugin:
+    """Represent a dynamic plugin."""
+
+    # pylint: disable=too-many-instance-attributes
+
     def __init__(self, config, interface, connection):
+        """Set up instance."""
         self.loop = asyncio.get_event_loop()
         self.config = dotdict(config)
         self.workspace = self.config.workspace
-        self.id = self.config.id or str(uuid.uuid4())
+        self.id = self.config.id or str(uuid.uuid4())  # pylint: disable=invalid-name
         self.name = self.config.name
         self.initializing = False
         self._disconnected = True
@@ -25,14 +31,17 @@ class DynamicPlugin:
         self.connection = connection
         self.authorizer = None
         self.api = None
+        self.running = False
+        self.terminating = False
 
         self._bind_interface(interface)
-        self.initializeIfNeeded(self.connection, self.config)
+        self.initialize_if_needed(self.connection, self.config)
 
         def initialized(data):
+            """Handle initialized message."""
             if "error" in data:
                 self.error(data["error"])
-                logger.error("Plugin failed to initialize", data["error"])
+                logger.error("Plugin failed to initialize: %s", data["error"])
                 raise Exception(data["error"])
 
             asyncio.ensure_future(self._setup_rpc(connection, data["config"]))
@@ -42,21 +51,22 @@ class DynamicPlugin:
 
     def _bind_interface(self, interface):
         self._initial_interface = dotdict(_rintf=True)
-        for k in interface:
-            if callable(interface[k]):
-                self._initial_interface[k] = partial(interface[k], self)
-            elif isinstance(interface[k], dict):
+        for key in interface:
+            if callable(interface[key]):
+                self._initial_interface[key] = partial(interface[key], self)
+            elif isinstance(interface[key], dict):
                 utils = dotdict()
-                for u in interface[k]:
-                    if callable(utils[u]):
-                        utils[u] = partial(interface[k][u], self)
-                interface[k] = utils
+                for util in interface[key]:
+                    if callable(utils[util]):
+                        utils[util] = partial(interface[key][util], self)
+                interface[key] = utils
             else:
-                self._initial_interface[k] = interface[k]
+                self._initial_interface[key] = interface[key]
 
-    async def _setup_rpc(self, connection, pluginConfig):
+    async def _setup_rpc(self, connection, plugin_config):
+        """Set up rpc."""
         self.initializing = True
-        logger.info(f'setting up imjoy-rpc for {pluginConfig["name"]}')
+        logger.info("Setting up imjoy-rpc for %s", plugin_config["name"])
         _rpc_context = ContextLocal()
         _rpc_context.api = self._initial_interface
         _rpc_context.default_config = {}
@@ -64,11 +74,11 @@ class DynamicPlugin:
         self._register_rpc_events()
         self._rpc.set_interface(self._initial_interface)
         await self._send_interface()
-        self._allow_execution = pluginConfig.get("allow_execution")
+        self._allow_execution = plugin_config.get("allow_execution")
         if self._allow_execution:
             await self._execute_plugin()
 
-        self.config.passive = self.config.passive or pluginConfig.get("passive")
+        self.config.passive = self.config.passive or plugin_config.get("passive")
         if self.config.passive:
 
             async def func(*args):
@@ -92,30 +102,41 @@ class DynamicPlugin:
         self._disconnected = False
         self.initializing = False
         logger.info(
-            f"Plugin loaded successfully (workspace={self.workspace}, name={self.name}, description={self.config.description}, api={list(self.api.keys())})"
+            "Plugin loaded successfully (workspace=%s, "
+            "name=%s, description=%s, api=%s)",
+            self.workspace,
+            self.name,
+            self.config.description,
+            list(self.api),
         )
 
     def error(self, *args):
+        """Log an error."""
         self._log_history.append({"type": "error", "value": args})
-        logger.error(f"Error in Plugin {self.id}: ${args}")
+        logger.error("Error in Plugin %s: $%s", self.id, args)
 
     def log(self, *args):
+        """Log."""
         if isinstance(args[0], dict):
             self._log_history.append(args[0])
-            logger.info(f"Plugin ${self.id}:{args[0]}")
+            logger.info("Plugin $%s:%s", self.id, args[0])
         else:
             msg = " ".join(map(str, args))
-            self._log_history.push({"type": "info", "value": msg})
-            logger.info(f"Plugin ${self.id}: ${msg}")
+            self._log_history.append({"type": "info", "value": msg})
+            logger.info("Plugin $%s: $%s", self.id, msg)
 
     def _set_disconnected(self):
+        """Set disconnected state."""
         self._disconnected = True
         self.running = False
         self.initializing = False
         self.terminating = False
 
     def _register_rpc_events(self):
+        """Register rpc events."""
+
         def disconnected(details):
+            """Handle disconnected."""
             if details:
                 if "error" in details:
                     self.error(details["message"])
@@ -125,54 +146,67 @@ class DynamicPlugin:
 
         self._rpc.on("disconnected", disconnected)
 
-        def remoteReady(_):
+        def remote_ready(_):
+            """Handle remote ready."""
             logger.info("Plugin interface updated")
 
-        self._rpc.on("remoteReady", remoteReady)
+        self._rpc.on("remoteReady", remote_ready)
 
         def remote_idle():
+            """Handle remote idle."""
             self.running = False
 
         self._rpc.on("remoteIdle", remote_idle)
 
         def remote_busy():
+            """Handle remote busy."""
             self.running = True
 
         self._rpc.on("remoteBusy", remote_busy)
 
     async def _execute_plugin(self):
+        """Execute plugin."""
         raise NotImplementedError
 
     def _send_interface(self):
+        """Send the interface."""
         fut = self.loop.create_future()
 
-        def interfaceSetAsRemote(result):
+        def interface_set_as_remote(result):
+            """Set interface as remote."""
             fut.set_result(result)
 
-        self._rpc._connection.once("interfaceSetAsRemote", interfaceSetAsRemote)
+        # pylint: disable=protected-access
+        self._rpc._connection.once("interfaceSetAsRemote", interface_set_as_remote)
         self._rpc.send_interface()
         return fut
 
     def _request_remote(self):
+        """Request remote."""
         fut = self.loop.create_future()
 
-        def remoteReady(result):
+        def remote_ready(result):
+            """Set remote ready."""
             try:
                 fut.set_result(self._rpc.get_remote())
             # TODO: this happens when the the plugin is reconnected
             except asyncio.exceptions.InvalidStateError:
                 pass
 
-        self._rpc.once("remoteReady", remoteReady)
+        self._rpc.once("remoteReady", remote_ready)
         self._rpc.request_remote()
         return fut
 
-    def initializeIfNeeded(self, connection, default_config):
-        def imjoyRPCReady(data):
+    @staticmethod
+    def initialize_if_needed(connection, default_config):
+        """Initialize if needed."""
+
+        def imjoy_rpc_ready(data):
+            """Handle rpc ready message."""
             config = data["config"] or {}
             forwarding_functions = ["close", "on", "off", "emit"]
-            type = config.get("type") or default_config.get("type")
-            if type in ["rpc-window", "window"]:
+            type_ = config.get("type") or default_config.get("type")
+            if type_ in ["rpc-window", "window"]:
                 forwarding_functions = forwarding_functions + [
                     "resize",
                     "show",
@@ -184,7 +218,8 @@ class DynamicPlugin:
             if config.get("credential_required"):
                 if isinstance(config.credential_fields, list):
                     raise Exception(
-                        "Please specify the `config.credential_fields` as an array of object."
+                        "Please specify the `config.credential_fields` "
+                        "as an array of object."
                     )
 
                 if default_config["credential_handler"]:
@@ -212,13 +247,14 @@ class DynamicPlugin:
                 }
             )
 
-        connection.once("imjoyRPCReady", imjoyRPCReady)
+        connection.once("imjoyRPCReady", imjoy_rpc_ready)
 
     async def terminate(self, force=False):
+        """Terminate."""
         try:
             if self.api and self.api.exit and callable(self.api.exit):
-                logger.info(f"Terminating plugin {self.workspace}/{self.name}...")
+                logger.info("Terminating plugin %s/%s", self.workspace, self.name)
                 self.api.exit()
         finally:
-            logger.info(f"Plugin {self.config.name} terminated.")
+            logger.info("Plugin %s terminated.", self.config.name)
             self._set_disconnected()
