8bd20a68f9d7235bf396af0a4b2d8054d4b59dde
Wei Ouyang <oeway007@gmail.com>
2021-04-26 15:18:42 +0200

    Add fastapi

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ except ImportError:
         "python-jose==3.2.0",
         "python-socketio[asyncio_client]==5.0.4",
         "pyyaml",
+        "fastapi>=0.63.0"
         "uvicorn>=0.13.4",
     ]
 
