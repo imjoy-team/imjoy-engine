4bea6e54eef1a31cdf01b4a9372ce0c7727fea89
Martin Hjelmare <marhje52@gmail.com>
2021-09-20 11:37:27 +0200

    Delint more

--- imjoy/__init__.py
+++ imjoy/__init__.py
@@ -6,7 +6,9 @@ from imjoy_rpc import *  # noqa F401, F403
 from imjoy_rpc import __all__ as imjoy_rpc_all
 
 # read version information from file
-VERSION_INFO = json.loads((Path(__file__).parent / "VERSION").read_text().strip())
+VERSION_INFO = json.loads(
+    (Path(__file__).parent / "VERSION").read_text(encoding="utf-8").strip()
+)
 __version__ = VERSION_INFO["version"]
 
 __all__ = imjoy_rpc_all
